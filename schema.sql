bf4a870784f1cc7e10a39d7955e4f350599f66da
Armando Contreras <armandocontreras62596@gmail.com>
2021-01-15 18:12:22 -0800

    added my database, it is working

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employees_DB;
+CREATE database employees_DB;
+
+USE employees_DB;
+
+CREATE TABLE department (
+  departmentID INT NOT NULL AUTO_INCREMENT,
+  depName VARCHAR(100) NULL,
+  PRIMARY KEY (departmentID)
+);
+
+CREATE TABLE roles (
+  rolesID INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL NULL,
+  departmentID INT,
+  PRIMARY KEY (rolesID),
+  FOREIGN KEY (departmentID) REFERENCES department(departmentID)
+);
+
+CREATE TABLE employee (
+  employeeID INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name DECIMAL NULL,
+  rolesID INT,
+  PRIMARY KEY (employeeID),
+  FOREIGN KEY (rolesID) REFERENCES roles(rolesID)
+);
+
+SELECT * FROM department;
+select * from roles;
+SELECT * FROM employee;
